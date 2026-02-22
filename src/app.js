8b02f978e166261a41fc3bff2caa081d6daa1684
Ujjwal Pathak <ujjwalpathak@Ujjwals-MacBook-Air.local>
2026-02-22 15:27:50 +0530

    final setup

--- src/app.js
+++ src/app.js
@@ -17,14 +17,4 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 
-// routes
-import userRouter from './routes/user.routes.js';
-
-
-app.use('/api/v1/users', userRouter);
-
-
-
-
-
 export default app;
